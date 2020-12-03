97b3b586f22a72ad927a7030361c2268b3fa7848
kshitij deopa <kshtjsngh0@gmail.com>
2020-12-03 00:46:11 -0600

    Kolaam

---
+++ app/src/main/java/com/example/kolaam/HomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.kolaam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import 	androidx.appcompat.widget.AppCompatButton;
+
+public class HomeActivity extends AppCompatActivity {
+    public Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, AdelActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
+
+
+
+
